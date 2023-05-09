cdf5cc389617767d8ec7ea10a7be261dff32d9e1
keyhong <wnhong96@naver.com>
2023-05-10 01:37:51 +0900

    Upload API-Key No Exist Version

---
+++ src/api_keys.py
@@ -0,0 +1,14 @@
+class RestApiKey:
+
+    __rest_api_key = [
+        'Message : Insert your kakao REST-API Keys'
+    ]
+
+    @property
+    def get_rest_api_key(self):
+        return self.__rest_api_key
+
+    def is_available(self):
+        pass
+
+    

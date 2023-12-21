55d59a66e763443783f7df0ce48e2c7b9201a571
Gyuwon Hong <wnhong96@gmail.com>
2023-12-22 04:22:50 +0900

    Update api_keys.py

--- src/api_keys.py
+++ src/api_keys.py
@@ -1,14 +1,13 @@
-class RestApiKey:
+#!/usr/bin/env python3
+from typing import List
 
-    __rest_api_key = [
+class RestApiKey:
+    """ Kakao RestfulAPI에 Get 메서드로 데이터를 가져오기 위해 request 파라미터로 넘기는 API Key """
+    
+    __rest_api_key: List[str] = [
         'Message : Insert your kakao REST-API Keys'
     ]
 
     @property
-    def get_rest_api_key(self):
+    def get_rest_api_key(self) -> List[str]:
         return self.__rest_api_key
-
-    def is_available(self):
-        pass
-
-    
